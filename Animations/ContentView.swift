39ffd9b2089d5ec4f660520d7cc1597179f6ba40
seahpark247 <famousjedo@gmail.com>
2025-04-21 09:20:03 -0500

    feat(ui): Add practice animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Coke") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+
+//        VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(
+//                .easeInOut(duration: 1)
+//                .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//            Button("Tap") {
+//                animationAmount += 1
+//            }
+//            .padding(10)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .scaleEffect(animationAmount)
+//            
+//            Spacer()
+//            
+//            Button("Tap Me") {
+//                //            animationAmount += 1
+//            }
+//            .padding(50)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            //        .scaleEffect(animationAmount)
+//            //        .blur(radius: (animationAmount - 1) * 3 )
+//            .overlay(
+//                Circle()
+//                    .stroke(.red)
+//                    .scaleEffect(animationAmount)
+//                    .opacity(2 - animationAmount)
+//                    .animation(
+//                        .easeOut(duration: 1)
+//                        .repeatForever(autoreverses: false)
+//                        ,
+//                        value: animationAmount)
+//            )
+//            .onAppear {
+//                animationAmount = 2
+//            }
+//        }
     }
 }
 
