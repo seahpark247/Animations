0ed21c911ca5a408d52f69ea927ce25b7648120a
seahpark247 <famousjedo@gmail.com>
2025-04-20 21:58:53 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Seah Park on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
